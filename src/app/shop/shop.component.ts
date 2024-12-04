0da42dc3e8ed53f6c64d2263c6e0cb197ee63378
Argentfire <Silv3rfire@hotmail.com>
2024-12-04 21:51:48 +0200

    Implemented product retrieval

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,54 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ShopService } from './shop.service';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [],
+  imports: [
+    ProductCardComponent,
+    CommonModule
+  ],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.scss'
 })
 export class ShopComponent {
+  imageSrc: string | ArrayBuffer | null = null;
+
+  honey: any[] = [];
+  polen: any[] = [];
+  propolis: any[] = [];
+  royalJelly: any[] = [];
   constructor(private shopService: ShopService) {
+  }
+
+  ngOnInit() {
+    this.shopService.getProducts().subscribe(
+      (data: any) => {
+        this.honey = data.honey;
+        this.polen = data.polen;
+        this.propolis = data.propolis;
+        this.royalJelly = data.royalJelly;
+      },
+      (error) => {
+
+      }
+    );
+
+    this.shopService.getImage().subscribe(
+      (data: any) => {
+        // debugger
+        this.imageSrc = data;
+        // const reader = new FileReader();
+        // reader.readAsDataURL(data);
+        // reader.onloadend = () => {
+        //   let result: any = reader.result;
+        //   this.imageSrc = result.replace('data:text/plain;', 'data:image/png;');
+        // };
+
+      }
+    );
   }
 
   @ViewChild('myDialog', { static: true }) dialogElement!: ElementRef<HTMLDialogElement>;
@@ -31,8 +70,10 @@ export class ShopComponent {
   }
 
   debug() {
-    const vm = this;
-    this.shopService.getProducts();
-    debugger
+    this.shopService.foo().subscribe(
+      response => {
+        var data = response;
+      }
+    );
   }
 }
