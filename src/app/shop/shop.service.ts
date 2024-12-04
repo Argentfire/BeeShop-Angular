fc2d20b24b60b7a621c1a039c9ebe32f17711a91
Argentfire <Silv3rfire@hotmail.com>
2024-12-04 21:50:53 +0200

    Implemented shop service

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -1,25 +1,51 @@
 import { Injectable } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
 
-  constructor(private httpClient: HttpClient) {
-
+  private baseURL: string = "https://localhost:7076";
+  constructor(
+    private httpClient: HttpClient) {
   }
 
-  public getProducts()  {
-    this.httpClient.get('https://localhost:7076/Honey')
-      .subscribe(
-        (response) => {
-          debugger
-        },
-        (error) => {
+  getHoney<T>() {
+    return this.httpClient.get(`${this.baseURL}/Honey`);
+  }
 
-        }
-    );
+  getProducts<T>() {
+    return this.httpClient.get(`${this.baseURL}/GetProducts`);
+  }
+
+  // getImage(): Observable<Blob> {
+  //   return this.httpClient.get(`${this.baseURL}/GetImage`, {responseType: 'blob'});
+  // }
+
+  getImage<T>() {
+    return this.httpClient.get(`${this.baseURL}/GetImage`);
+  }
+
+  foo(): Observable<any> {
+    const obj = {
+      productId: '5',
+      productType: 'Honey',
+      imagePath: 'D:\foo.jpg'
+    };
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    return this.httpClient.post(`${this.baseURL}/SetProductImage`, obj);
+  }
+
+  getHoneyImages<T>(prodId: string) {
+    // return this.httpClient.get(`${this.baseURL}/Honey/GetImages/${prodId}`);
+    return this.httpClient.get(`${this.baseURL}/Honey/GetImages/${prodId}`);
+  }
+
+  test<T>(url: string) {
+    return this.httpClient.get(url);
   }
 }
