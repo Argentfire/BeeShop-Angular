de17874d7f36af90eacf2024d4b49cef261f1530
Argentfire <Silv3rfire@hotmail.com>
2024-12-21 16:11:15 +0200

    Added toastr package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, HttpClientModule } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
-  provideHttpClient()
+    provideHttpClient(),
+    provideAnimations(),
+    provideToastr(),
   ]
 };
