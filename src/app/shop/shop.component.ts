10a390af98e30460ac57b1e919d17b06b811b44a
Argentfire <Silv3rfire@hotmail.com>
2024-12-23 21:41:59 +0200

    Add product handling and thumbnail fetching in ShopComponent

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -2,16 +2,14 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ShopService } from './shop.service';
 import { ProductCardComponent } from './product-card/product-card.component';
 import { CommonModule } from '@angular/common';
+import { Product } from './../models/product';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [
-    ProductCardComponent,
-    CommonModule
-  ],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './shop.component.html',
-  styleUrl: './shop.component.scss'
+  styleUrl: './shop.component.scss',
 })
 export class ShopComponent {
   imageSrc: string | ArrayBuffer | null = null;
@@ -20,7 +18,11 @@ export class ShopComponent {
   polen: any[] = [];
   propolis: any[] = [];
   royalJelly: any[] = [];
+  products: Product[];
+  @ViewChild('myDialog', { static: true })
+  dialogElement!: ElementRef<HTMLDialogElement>;
   constructor(private shopService: ShopService) {
+    this.products = [];
   }
 
   ngOnInit() {
@@ -30,50 +32,96 @@ export class ShopComponent {
         this.polen = data.polen;
         this.propolis = data.propolis;
         this.royalJelly = data.royalJelly;
+
+        for (let honey of data.honey) {
+          const prod = new Product(
+            honey.id,
+            honey.name,
+            honey.description,
+            honey.price,
+            honey.amountLeft,
+            honey.weight,
+            honey.type,
+            honey.images
+          );
+          this.products.push(prod);
+          this.getProductThumbnails(prod);
+        }
+        for (let polen of data.polen) {
+          this.products.push(
+            new Product(
+              polen.id,
+              polen.name,
+              polen.description,
+              polen.price,
+              polen.amountLeft,
+              polen.weight,
+              polen.type,
+              polen.images
+            )
+          );
+        }
+        for (let propolis of data.propolis) {
+          this.products.push(
+            new Product(
+              propolis.id,
+              propolis.name,
+              propolis.description,
+              propolis.price,
+              propolis.amountLeft,
+              propolis.weight,
+              propolis.type,
+              propolis.images
+            )
+          );
+        }
+        for (let royalJelly of data.royalJelly) {
+          this.products.push(
+            new Product(
+              royalJelly.id,
+              royalJelly.name,
+              royalJelly.description,
+              royalJelly.price,
+              royalJelly.amountLeft,
+              royalJelly.weight,
+              royalJelly.type,
+              royalJelly.images
+            )
+          );
+        }
       },
-      (error) => {
-
-      }
-    );
-
-    this.shopService.getImage().subscribe(
-      (data: any) => {
-        // debugger
-        this.imageSrc = data;
-        // const reader = new FileReader();
-        // reader.readAsDataURL(data);
-        // reader.onloadend = () => {
-        //   let result: any = reader.result;
-        //   this.imageSrc = result.replace('data:text/plain;', 'data:image/png;');
-        // };
-
-      }
+      (error) => {}
     );
   }
 
-  @ViewChild('myDialog', { static: true }) dialogElement!: ElementRef<HTMLDialogElement>;
+  getProductThumbnails(product: Product) {
+    this.shopService.getThumbnail(product.id).subscribe((data: any) => {
+      if (data) {
+        if (data.url) {
+          product.thumbnail = data.url;
+        }
+      }
+    });
+  }
 
   openDialog() {
-    debugger
-      // Check if dialogElement is defined before using it
-      if (this.dialogElement) {
-          const dialog: HTMLDialogElement = this.dialogElement.nativeElement;
-          dialog.showModal();
-      }
+    // Check if dialogElement is defined before using it
+    if (this.dialogElement) {
+      const dialog: HTMLDialogElement = this.dialogElement.nativeElement;
+      dialog.showModal();
+    }
   }
 
   closeDialog() {
-      if (this.dialogElement) {
-          const dialog: HTMLDialogElement = this.dialogElement.nativeElement;
-          dialog.close();
-      }
+    if (this.dialogElement) {
+      const dialog: HTMLDialogElement = this.dialogElement.nativeElement;
+      dialog.close();
+    }
   }
 
   debug() {
-    this.shopService.foo().subscribe(
-      response => {
-        var data = response;
-      }
-    );
+    this.shopService.foo().subscribe((response) => {
+      var data = response;
+    });
   }
 }
