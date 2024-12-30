b24351deba6da5aa3c67e9f54c72f42a4767bb35
Argentfire <Silv3rfire@hotmail.com>
2024-12-30 16:31:20 +0200

    Add typeText property and setTypeText method to Product model for better type representation

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -6,6 +6,7 @@ export class Product {
   amountLeft: number;
   weight: number;
   type: number;
+  typeText!: string;
   thumbnail: string;
   constructor(id?: string, name?: string, description?: string, price?: number, amountLeft?: number, weight?: number, type?: number, thumbnail?: string) {
     this.id = id || '';
@@ -14,7 +15,36 @@ export class Product {
     this.price = price || 0;
     this.amountLeft = amountLeft || 0;
     this.weight = weight || 0;
-    this.type = type || -1;
+    this.type = type ?? -1;
     this.thumbnail = thumbnail || '';
+
+    this.setTypeText();
   }
+
+  setTypeText() {
+    switch (this.type) {
+      case ProductType.Honey:
+        this.typeText = 'Пчелен Мед';
+        break;
+      case ProductType.Polen:
+        this.typeText = 'Пчелен Прашец';
+        break;
+      case ProductType.Propolis:
+        this.typeText = 'Прополис';
+        break;
+      case ProductType.RoyalJelly:
+        this.typeText = 'Пчелно Млечице';
+        break;
+      default:
+        this.typeText = '';
+        break;
+    }
+  }
+}
+
+export enum ProductType {
+  Honey = 0,
+  Polen = 1,
+  Propolis = 2,
+  RoyalJelly = 3
 }
